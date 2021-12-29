fc6a32c877e61ca9acff206047d01e8f0d290bb2
Adam Tse <mr.adam.tse@gmail.com>
2021-12-29 16:12:24 +0800

    setup vite

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        MEOW
+      </header>
+    </div>
+  )
+}
+
+export default App
